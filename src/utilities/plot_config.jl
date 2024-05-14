b1cac646dcb7d51d2e95c9b379e026f2fce16f72
Robin Armstrong <rja243@cornell.edu>
2024-05-14 11:30:38 -0400

    Changed the font configuration for plots, changed the number of ranks tested in the Kahan example.

--- src/utilities/plot_config.jl
+++ src/utilities/plot_config.jl
@@ -1,3 +1,9 @@
+using PyPlot
+using PyCall
+
+rcParams = PyDict(matplotlib["rcParams"])
+rcParams["font.family"] = "serif"
+
 algnames   = ["rsvd_q0", "rsvd_q1", "rgks", "rid", "levg"]
 alglabels  = Dict("rsvd_q0" => "RSVD (0 iterations)", "rsvd_q1" => "RSVD (1 iteration)", "rgks" => "RGKS", "rid" => "RID", "levg" => "LEVG")
 algcolors  = Dict("rsvd_q0" => "blue", "rsvd_q1" => "magenta", "rgks" => "brown", "rid" => "darkslategrey", "levg" => "forestgreen")
